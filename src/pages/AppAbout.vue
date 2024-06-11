9e99dc7e72a75232d2a5a7436576e76effbf943f
chidoriii7 <muratore.danilo@yahoo.it>
2024-06-11 11:03:27 +0200

    no errors version

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -5,13 +5,11 @@ export default {
 </script>
 
 <template>
-<<<<<<< HEAD
-    <h1>ciao sono about di danilo</h1>
-=======
 
-    <h1>ciao sono about di danilo</h1>
-
->>>>>>> b0071e1af3df94d7f18da52bac1430832409c54f
+    <h1>
+        ciao sono about di danilo
+    </h1>
+    
 </template>
 
 
