d6d1fb3106759a8ecefd26f40cb8e50cbe3ba383
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-11 10:51:52 +0200

    Push fix

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -5,13 +5,12 @@ export default {
 </script>
 
 <template>
-<<<<<<< HEAD
-    <h1>ciao sono about di danilo</h1>
-=======
+    <<<<<<< <h1>ciao sono about di danilo</h1>
+        =======
 
-    <h1>ciao sono about di danilo</h1>
+        <h1>ciao sono about di danilo</h1>
 
->>>>>>> b0071e1af3df94d7f18da52bac1430832409c54f
+        >>>>>>> b0071e1af3df94d7f18da52bac1430832409c54f
 </template>
 
 
