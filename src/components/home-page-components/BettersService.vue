df1048d5d31640259375a7bee6ccfea58394052e
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-11 14:47:50 +0200

    [upd] BetterService component Beta

---
+++ src/components/home-page-components/BettersService.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'BetterService'
+}
+
+</script>
+
+<template>
+    <section class="bg-image">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-12 py-5">
+                    <div class="text text-center py-5">
+                        <h2>Yacht Charters, Repairs<br>& Transportation, US-wide!</h2>
+                        <button>More Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.bg-image {
+    background-image: url('https://ld-wp73.template-help.com/wordpress/prod_5291/v1/wp-content/uploads/2020/12/bg5.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+
+
+}
+</style>
