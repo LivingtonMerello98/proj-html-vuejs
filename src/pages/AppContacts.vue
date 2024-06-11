d0738ee3d4176cfbedcd6b88c03feb3327f7372a
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-11 13:49:40 +0200

    [UPD] added maps in Appmaps component

--- src/pages/AppContacts.vue
+++ src/pages/AppContacts.vue
@@ -1,13 +1,22 @@
 <script>
+import AppForm from '../components/contacts-page-componets/AppForm.vue';
+import AppMaps from '../components/contacts-page-componets/AppMaps.vue';
 export default {
-    name: "AppContacts"
+    name: "AppContacts",
+    components: {
+        AppForm,
+        AppMaps
+    }
 }
 </script>
 
 <template>
-    <h1>AppContacts di Vincenzo</h1>
-    <i class="fa-solid fa-star"></i>
-    <p>ciao mondo </p>
+    <AppMaps></AppMaps>
+    <AppForm></AppForm>
+    <!-- <h1>AppContacts di Vincenzo</h1 -->
+    <!-- <i class="fa-solid fa-star"></i> -->
+    <!-- <p>ciao mondo </p> -->
+    <!-- <p>ciao </p> -->
 </template>
 
 <style></style>
