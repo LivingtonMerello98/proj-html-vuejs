67cc5b28ccf31ae6d6dda7588d5b6b960e35cfc8
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-13 12:45:34 +0200

    [UPD] fix

--- src/components/about-page-components/AppOurTeam.vue
+++ src/components/about-page-components/AppOurTeam.vue
@@ -1,7 +1,7 @@
 <script>
-    export default {
+export default {
     name: "AppOurTeam"
-    
+
 }
 </script>
 
@@ -24,7 +24,10 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info py-3">
-                                <p>Mr. Buoyega was born in one of Detroit’s poorest blocks, all before pursuing his successful managerial career in the future. For him, the breakthrough happened when he was able to get on Harvard University’s Law School list. After graduating from there, he was working for a few years for a…</p>
+                                <p>Mr. Buoyega was born in one of Detroit’s poorest blocks, all before pursuing his
+                                    successful managerial career in the future. For him, the breakthrough happened when he
+                                    was able to get on Harvard University’s Law School list. After graduating from there, he
+                                    was working for a few years for a…</p>
                             </div>
                         </div>
                     </div>
@@ -42,7 +45,10 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info py-3">
-                                <p>A talented and creative leader, Kenneth is known among his colleagues for how good his organizing and team leading skills are… Working priorly for a number of American and International multi-billion dollar companies, Kenneth has built up a reputation and a skillset worthy of an…</p>
+                                <p>A talented and creative leader, Kenneth is known among his colleagues for how good his
+                                    organizing and team leading skills are… Working priorly for a number of American and
+                                    International multi-billion dollar companies, Kenneth has built up a reputation and a
+                                    skillset worthy of an…</p>
                             </div>
                         </div>
                     </div>
@@ -60,7 +66,10 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info py-3">
-                                <p>Being as a devoted family man as he is, it is incredible how Jonathan has time left for managing multi-million dollar contracts, satisfying all of our clients and managing a vast team of professionals under his command. His resolve is well-known, making him one of the few executive…</p>
+                                <p>Being as a devoted family man as he is, it is incredible how Jonathan has time left for
+                                    managing multi-million dollar contracts, satisfying all of our clients and managing a
+                                    vast team of professionals under his command. His resolve is well-known, making him one
+                                    of the few executive…</p>
                             </div>
                         </div>
                     </div>
@@ -78,7 +87,10 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info py-3">
-                                <p>MOriginally from London, at one point of his sophomore college year, Martin decided to turn the tables for his career and travel to the US, to try and get a Harvard MBA degree… Eventually, he succeeded in that, becoming one of the most respected executive managers in his field of work.</p>
+                                <p>MOriginally from London, at one point of his sophomore college year, Martin decided to
+                                    turn the tables for his career and travel to the US, to try and get a Harvard MBA
+                                    degree… Eventually, he succeeded in that, becoming one of the most respected executive
+                                    managers in his field of work.</p>
                             </div>
                         </div>
                     </div>
@@ -96,7 +108,10 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info py-3">
-                                <p>A Juneau, Alaska native, Mrs. Applebaum has come a long way since her high school graduation all the way up to getting a Brown University Master’s degree in Economics. Talented with the digits, she’s no less successful when it comes to managing the human resources of the company. Ask anyone at our office who…</p>
+                                <p>A Juneau, Alaska native, Mrs. Applebaum has come a long way since her high school
+                                    graduation all the way up to getting a Brown University Master’s degree in Economics.
+                                    Talented with the digits, she’s no less successful when it comes to managing the human
+                                    resources of the company. Ask anyone at our office who…</p>
                             </div>
                         </div>
                     </div>
@@ -114,7 +129,11 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info py-3">
-                                <p>Born in New York City, Elisa always felt a strong urge for being an ambitious overachiever. Eventually, by the age of 25 she already had received her first Wall Street executive position, making her one of the youngest managers in the company. With her overall work experience spanning over 2 decades, Mrs. Hansolo is definitely the kind of a senior…</p>
+                                <p>Born in New York City, Elisa always felt a strong urge for being an ambitious
+                                    overachiever. Eventually, by the age of 25 she already had received her first Wall
+                                    Street executive position, making her one of the youngest managers in the company. With
+                                    her overall work experience spanning over 2 decades, Mrs. Hansolo is definitely the kind
+                                    of a senior…</p>
                             </div>
                         </div>
                     </div>
@@ -132,7 +151,10 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info ">
-                                <p>If you’d ask any of our workers, who is the executive manager that they’re always looking up and living up to, most likely Emily’s name will surface. Having a vast and thorough practical working experience lasting for more than 18 years, Mrs. Bluesome is an ideal manager, who is always…</p>
+                                <p>If you’d ask any of our workers, who is the executive manager that they’re always looking
+                                    up and living up to, most likely Emily’s name will surface. Having a vast and thorough
+                                    practical working experience lasting for more than 18 years, Mrs. Bluesome is an ideal
+                                    manager, who is always…</p>
                             </div>
                         </div>
                     </div>
@@ -150,30 +172,30 @@
                                 <span>Manager</span>
                             </div>
                             <div class="member-info py-3">
-                                <p>If you’d ever need to figure out a workaround for a complicated business problem, look no further than Diane’s desk for getting a thorough and all-rounded advice. She has been working in the industry for more than 20 years, knowing all the ins and outs perfectly and teaching each member of our team. Regardless of which exact type…</p>
+                                <p>If you’d ever need to figure out a workaround for a complicated business problem, look no
+                                    further than Diane’s desk for getting a thorough and all-rounded advice. She has been
+                                    working in the industry for more than 20 years, knowing all the ins and outs perfectly
+                                    and teaching each member of our team. Regardless of which exact type…</p>
                             </div>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
-    </section>
-</template>
+    </div>
+</section></template>
 
-<style lang="scss" scoped>
-    @use 'src/assets/partials/mixin' as*;
-    @use 'src/assets/partials/variables' as*;
+<style lang="scss" scoped>@use 'src/assets/partials/mixin' as*;
+@use 'src/assets/partials/variables' as*;
 
-    section{
-        background-color: #F8F9FD;
-    }
-    
-    h2 {
-        font-weight: bold;
-        font-size: $custom-title-section-size
-    }
+section {
+    background-color: #F8F9FD;
+}
 
-    .member-name{
-        color: #09C2DD;
-    }
-</style>
+h2 {
+    font-weight: bold;
+    font-size: $custom-title-section-size
+}
+
+.member-name {
+    color: #09C2DD;
+}</style>
