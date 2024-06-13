bbd6165493b4dca5b08810075b362bed153d3d26
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-13 09:28:49 +0200

    [UPD] AppUniversalHeader appuniversalheader made responsive

--- src/components/AppUniversalHeader.vue
+++ src/components/AppUniversalHeader.vue
@@ -11,35 +11,33 @@ export default {
   <section>
     <div class="container">
       <div class="row">
-        <div class="col-8 py-2 d-flex">
+        <div class="col-lg-8 col-12 py-2 d-flex flex-column flex-lg-row align-items-center">
 
           <!-- INDIRIZZO E NUMERO DI TELEFONO HEADER -->
 
-          <div class="address">
+          <div class="address mb-2 mb-lg-0">
             <span>
               <i class="fa-solid fa-location-dot map-location-icon"></i>
             </span>
             <span class="mx-3 custom-font-size">7087 Richmond hwy, Alexandria, VA</span>
           </div>
-          <div class="telephone-contact mx-5">
+          <div class="telephone-contact mx-0 mx-lg-5">
             <span><i class="fa-solid fa-phone phone-icon"></i></span>
             <span class="mx-3 custom-font-size">800-2345-6789</span>
           </div>
         </div>
-        <div class="col-4 d-flex align-items-center justify-content-end">
+        <div class="col-lg-4 col-12 d-flex align-items-center justify-content-center justify-content-lg-end">
           <div class="social-icon-circle ">
             <a href=""><i class="fa-brands fa-facebook "></i></a>
             <a href=""><i class="fa-brands fa-twitter mx-4"></i></a>
             <a href=""><i class="fa-brands fa-instagram "></i></a>
           </div>
         </div>
-
-
       </div>
     </div>
-
   </section>
 </template>
+
 
 <style lang="scss" scoped>
 @use "src/assets/partials/variables" as *;
